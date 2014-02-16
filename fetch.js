0d7aced5a51fb0be50ea1fecbca592d240930579
Artem Vovsya <avovsya@yandex-team.ru>
2014-02-16 21:53:26 +0200

    - Basic template  - Final news parser

--- fetch.js
+++ fetch.js
@@ -2,22 +2,45 @@ var http = require('http'),
     Parser = require('feedparser'),
     parser = new Parser();
 
-exports = function (callback) {
 
-};
+module.exports = function fetch (callback) {
+    var bbcNews = [],
+        skyNews = [],
+        bbcParser = new Parser(),
+        skyParser = new Parser(),
+        parserEnd = 0;
 
-http.get({ host: 'feeds.bbci.co.uk', port: 80, path: '/news/rss.xml' }, function (res) {
-    res.pipe(parser);
-}).on('error', function () {
-    console.log('Something went wrong while requesting');
-});
+    http.get({ host: 'feeds.bbci.co.uk', port: 80, path: '/news/rss.xml' }, function (res) {
+        res.pipe(bbcParser);
+    }).on('error', function () {
+        console.log('Something went wrong while requesting BBC.COM');
+    });
 
-parser.on('readable', function () {
-    while(headline = this.read()) {
-        console.log(headline);
+    http.get({ host: 'news.sky.com', port: 80, path: '/feeds/rss/home.xml' }, function (res) {
+        res.pipe(skyParser);
+    }).on('error', function () {
+        console.log('Something went wrong while requesting SKY.COM');
+    });
+
+    bbcParser.on('readable', function () {
+        while(headline = this.read()) {
+            bbcNews.push(headline);
+        }
+    });
+
+    skyParser.on('readable', function () {
+        while(headline = this.read()) {
+            skyNews.push(headline);
+        }
+    });
+
+    function parserFinished() {
+        parserEnd++;
+        if(parserEnd >= 2) {
+            callback(bbcNews, skyNews);
+        }
     }
-});
 
-parser.on('error', function () {
-    console.log('Something went wrong while parsing');
-});
+    bbcParser.on('end', parserFinished);
+    skyParser.on('end', parserFinished);
+};
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var http = require('http'),
-    EJS = require('ejs'),
-    fetch = require('fetch');
+    ejs = require('ejs'),
+    fetch = require('./fetch'),
+    fs = require('fs'),
+    html = fs.readFileSync('./index.ejs', 'utf8');
 
 http.createServer(function (req, res) {
-    fetch(function (data) {
-        var html = new EJS({ url: './index.ejs' }).render(data);
+    fetch(function (bbc, sky) {
+        var content = ejs.render(html, {
+            bbc: bbc.slice(0, 10),
+            sky: sky.slice(0, 10)
+        });
+
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(content, 'utf-8');
     });
