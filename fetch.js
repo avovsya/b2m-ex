3c401f06b4f21806aa83d095b01b46f020ceb3a3
Artem Vovsya <avovsya@yandex-team.ru>
2014-02-16 20:54:16 +0200

    Initial commit. Basic feed parsing using feedparser

---
+++ fetch.js
@@ -0,0 +1,19 @@
+var http = require('http'),
+    Parser = require('feedparser'),
+    parser = new Parser();
+
+http.get({ host: 'feeds.bbci.co.uk', port: 80, path: '/news/rss.xml' }, function (res) {
+    res.pipe(parser);
+}).on('error', function () {
+    console.log('Something went wrong while requesting');
+});
+
+parser.on('readable', function () {
+    while(headline = this.read()) {
+        console.log(headline);
+    }
+});
+
+parser.on('error', function () {
+    console.log('Something went wrong while parsing');
+});
